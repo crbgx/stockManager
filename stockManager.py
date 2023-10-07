f3f73b9f4a091f1c7ab9aef57228155115711591
CBGX <carlosbgx@hotmail.com>
2023-10-08 01:56:12 +0200

    Working, better

--- stockManager.py
+++ stockManager.py
@@ -1,16 +1,9 @@
 #bin/usr/stockManager
 
 #### Input data ####
-producedDay = [5, 0, 2, 5]
+producedDay = [5, 0, 3, 5]
 consumedDay = [4, 1, 3, 1]
 expectedDuration = 2    # Minimum value: 1
-
-
-#### Variables ####
-thrownAway = 0
-toConsume = consumedDay
-stock = [0] * (expectedDuration-1)   # Product goes off in: [n, n-1, n-2 ... 2, 1, 0] days
-
 
 
 
@@ -18,7 +11,15 @@ stock = [0] * (expectedDuration-1)   # Product goes off in: [n, n-1, n-2 ... 2, 
 def error_demand(index):
     print('Error: Demand cannot be satisfied')
     print(f'This happened at day: {index}')
+    print(f'Necessary stock missing: {consumedDay[index]}')
     end_program()
+
+
+
+def success(stock, thrownAway):
+    print('Simulation finished successfully')
+    print(f'Final stock: {stock}')
+    print(f'Thrown away: {thrownAway}')
 
 
 
@@ -44,51 +45,50 @@ def check_data():
             end_program()
 
 
+def stockManager(consumedDay, producedDay, expectedDuration):
+    # Variables init
+    stock = [0] * (expectedDuration - 1)   # Product goes off in: [n, n-1, n-2 ... 2, 1, 0] days
+    thrownAway = 0
 
-check_data()
-for index, i in enumerate(producedDay):
-    # We first calculate the stock array by adding the producedDay to the previous stock
-    stock = [producedDay[index]] + stock
-    thrownAway = thrownAway + stock[-1]
-    if index != 0:
-        del stock[-1]
-    print(stock)
-
-
-    # First day special case
-    if index == 0:  
-        stock[index] = producedDay[index] - consumedDay[index]
-        if stock[index] >= 0:
-            print(f'We have satisfied demand for day: {index} with stock with {expectedDuration} days left')
-            print(stock)
-        else:
-            error_demand(index)
+    check_data()
+    for index, i in enumerate(producedDay):
+        # We first calculate the stock array by adding the producedDay to the previous stock
+        stock = [producedDay[index]] + stock
+        thrownAway = thrownAway + stock[-1]
+        if index != 0:
+            del stock[-1]
 
 
-    else:
-        # Remove the ConsumedDay from the stock array
-        for j in range(0, len(stock)):
-            k = -j-1
-            print(k)
-            print(f'stock {stock[k]}')
-            print(f'toConsume {toConsume[index]}')
-            if stock[k] >= toConsume[index]:
-                stock[k] = stock[k] - toConsume[index]
-                toConsume[index] = 0
-                print(f'We have satisfied demand for day: {index} with stock with {-k} days left')
-                print(stock)
-                break
+        # First day special case
+        if index == 0:  
+            stock[index] = producedDay[index] - consumedDay[index]
+            if stock[index] >= 0:
+                print(f'We have satisfied demand for day: {index} with stock with {expectedDuration} days left')
+
             else:
-                print('im else')
-                toConsume[index] = toConsume[index] - stock[k]
-                stock[k] = 0
-
-            if j ==len(stock)-1 and toConsume[index] != 0:
                 error_demand(index)
 
 
+        else:
+            for j in range(0, len(stock)):
+                k = -j-1
+                if stock[k] >= consumedDay[index]:
+                    stock[k] = stock[k] - consumedDay[index]
+                    consumedDay[index] = 0
+                    print(f'We have satisfied demand for day: {index} with stock with {-k} days left')
+                    break
+
+                else:
+                    consumedDay[index] = consumedDay[index] - stock[k]
+                    stock[k] = 0
+
+                # Check if we run out of stock
+                if j == len(stock)-1 and consumedDay[index] != 0:
+                    error_demand(index)
+
+    success(stock, thrownAway)
 
 
-print('Simulation finished satisfactorily')
-print(f'Final stock: {stock}')
-print(f'Thrown away: {thrownAway}')
+
+#### Main ####
+stockManager(consumedDay, producedDay, expectedDuration)
